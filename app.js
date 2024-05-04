43263d7ee419e9d4ee8fc77ee982b76eb5d500a3
Nataliesmyth <nataliesmyth11@gmail.com>
2024-05-04 03:51:21 -0400

    mobile active state mvp complete

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const shareButton = document.getElementById('shareBtn');
-const label = document.getElementById('author')
+const label = document.getElementById('author');
 const shareContainer = document.getElementById('shareContainer')
+const shareActive = document.getElementById('shareActive')
 const socials = document.getElementById('socialLinks')
 
 function handleClick() {
     console.log('share button clicked!')
-    label.classList.add('hidden')
+    shareContainer.classList.add('hidden');
     shareContainer.style.background = '#48556A';
     shareButton.style.background = '#6E8098';
     shareButton.classList.add('share-btn-dark');
-    socials.classList.remove('hidden')
+    socials.classList.remove('hidden');
+    shareActive.classList.remove('hidden')
 }
 
 shareButton.addEventListener('click', handleClick)
